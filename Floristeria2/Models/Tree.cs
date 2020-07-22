e875a7680c512203c01998dc914aa18dcb9eca55
Francisco Gil Mayo <francisco.gil.mayo@gmail.com>
2020-07-22 10:08:08 +0200

    Primer commit

---
+++ Floristeria2/Models/Tree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Floristeria2.Models
+{
+    class Tree
+    {
+        public string Nombre { get; set; }
+        public string Altura { get; set; }
+        public string Precio { get; set; }
+
+        //Constructor
+
+        public Tree(TextBox nombre, TextBox altura, TextBox precio)
+        {
+            this.Nombre = nombre.ToString();
+            this.Altura = altura.ToString();
+            this.Precio = precio.ToString();
+        }
+    }
+}
